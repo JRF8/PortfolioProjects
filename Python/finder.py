d607adefe54dbd67531962aa9752c2f6cc76982a
Jesse Frazier <jesse.frazier.8@gmail.com>
2019-08-06 14:19:00 -0400

    continued progress on finder.py, adding test file as well

--- Python/finder.py
+++ Python/finder.py
@@ -1,23 +1,44 @@
 import re
-from array import array
+import os
 
 def main():
     f1 = openOriginal()
-    loopFile(f1)
+    origLineArray = loopFile(f1)
+    newLineArray = loopOrigLineArray()
+
+def loopOrigLineArray()
+    patProgChange = compProgChange()
+    patLineChange = compLineChange()
+    #flg will indicate if we are modifying the contents of the patch in this area.
+    flg = False
+    for i,line in enumerate(lineArray):
+        #print("line" + str(i) + ": " + line)
+        if regexMatch(patProgChange, line):
+            nextLine = lineArray[i+1]
+            flg = isNprLogic(nextLine)
+        else
+            #here is where I am going to process the non-header lines..
+            #i'll need to make the zrepl changes and the line number changes
+
+def isNprLogic(nextLine):
+    if nextLine.endswith(".npr-logic"):
+        return True
+    else:
+        return False
 
 def openOriginal():
     #open a file
     return open("test1.patch","r")
 
-
 def loopFile(f):
+    lineArray = []
     for x in f:
-        regexMatch(x)
+        lineArray.append(x)
+    return lineArray
 
-def regexMatch(x):
+def regexMatch(pattern, x):
     #x is a single line in the file
     #@@ -11,6 +11,6 @@ ohh, its line 10 now
-    pattern = compiledPattern()
     #the match() function will apply the pre-compiled regex called pattern to the string to search (x)
     match = pattern.match(x)
     if match:
@@ -25,17 +46,22 @@ def regexMatch(x):
     else:
         return
 
-def compiledPattern():
+def compLineChange():
     #@@ -11,6 +11,6 @@ ohh, its line 10 now
-    # the following line compiles a regular expression ahead of time for use later
+    # compilation for regex to find line change denotations:
     return re.compile("([\D]+)(-)([\d]+),([\d]+)([\D]+)(\+)([\d]+),([\d]+)([\D]+)")
+
+def compProgChange():
+    #compilation for regex to find program changes:
+    return re.compile("^[Ii]ndex")
 
 def processLine(match):
     #match.groups() returns an iterable list of the pieces of the match
     a = []
     for i, s in enumerate(match.groups()):
         if representsInt(s):
-            a.append(str(int(s)+1))
+            v = addToLineNums(s)
+            a.append(v)
         else:
             a.append(s)
     print(a)
@@ -46,6 +72,8 @@ def representsInt(s):
         return True
     except ValueError:
         return False
-#def addOne(myIntStr):
-    #return str(int(myIntStr) + 1)
+
+def addToLineNums(myStr):
+    return str(int(myStr)+2)
+
 main()
