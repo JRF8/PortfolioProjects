5fe41f5661b205f4864946b1710668c3a301cda5
Jesse Frazier <jesse.frazier.8@gmail.com>
2019-08-06 22:12:12 -0400

    Successfully able to split the .patch file into separate lists

--- Python/finder.py
+++ Python/finder.py
@@ -3,22 +3,35 @@ import os
 
 def main():
     f1 = openOriginal()
-    origLineArray = loopFile(f1)
-    newLineArray = loopOrigLineArray()
+    origLineList = loopFile(f1)
+    newLineList = loopOrigLineList(origLineList)
 
-def loopOrigLineArray()
+def loopOrigLineList(lineList):
     patProgChange = compProgChange()
     patLineChange = compLineChange()
+    newLineList = []
+    newLineSubList = []
     #flg will indicate if we are modifying the contents of the patch in this area.
-    flg = False
-    for i,line in enumerate(lineArray):
+    for line in lineList:
         #print("line" + str(i) + ": " + line)
         if regexMatch(patProgChange, line):
-            nextLine = lineArray[i+1]
-            flg = isNprLogic(nextLine)
-        else
-            #here is where I am going to process the non-header lines..
-            #i'll need to make the zrepl changes and the line number changes
+            newLineList = appendAList(newLineList, newLineSubList)
+            newLineSubList = []
+            newLineSubList.append(line)
+        else:
+            newLineSubList.append(line)
+    newLineList = appendAList(newLineList, newLineSubList)
+    for subList in newLineList:
+        print(subList)
+        
+def appendAList(a1, a2):
+    #a2 is the list to append to the target list, a1
+    #don't want to append a2 to a1 if a2 is empty
+    if not a2:
+        return []
+    else:
+        a1.append(a2)
+    return a1
 
 def isNprLogic(nextLine):
     if nextLine.endswith(".npr-logic"):
@@ -31,10 +44,10 @@ def openOriginal():
     return open("test1.patch","r")
 
 def loopFile(f):
-    lineArray = []
+    lineList = []
     for x in f:
-        lineArray.append(x)
-    return lineArray
+        lineList.append(x)
+    return lineList
 
 def regexMatch(pattern, x):
     #x is a single line in the file
@@ -42,9 +55,9 @@ def regexMatch(pattern, x):
     #the match() function will apply the pre-compiled regex called pattern to the string to search (x)
     match = pattern.match(x)
     if match:
-        processLine(match)
+        return True
     else:
-        return
+        return False
 
 def compLineChange():
     #@@ -11,6 +11,6 @@ ohh, its line 10 now
@@ -53,7 +66,7 @@ def compLineChange():
 
 def compProgChange():
     #compilation for regex to find program changes:
-    return re.compile("^[Ii]ndex")
+    return re.compile("^[Dd]iff")
 
 def processLine(match):
     #match.groups() returns an iterable list of the pieces of the match
