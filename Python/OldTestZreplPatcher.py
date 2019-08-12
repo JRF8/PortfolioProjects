339c0f56ac1a34767c039aea23d0af41ed46b7b4
Jesse Frazier <jesse.frazier.8@gmail.com>
2019-08-11 23:47:45 -0400

    grabbing change to historical code from new_feature

--- Python/OldTestZreplPatcher.py
+++ Python/OldTestZreplPatcher.py
@@ -5,6 +5,7 @@ import sys
 #----FUNCTION DEFINITIONS-------
 def main():
     find_replace()
+    
 def find_replace():
     for line in fileinput.FileInput("test.txt", inplace=1):
         char = line[0]
@@ -17,5 +18,14 @@ def find_replace():
             sys.stdout.write(char + line[1:])
         else:
             sys.stdout.write(line)
+#-------PSEUDO CODE----------
+# connect to SVN, get revisions
+# from revisions, generate patches
+# also, recognize and grab appropriate code
+# create working directory, place code in there
+# also have parallel backup directory with copy of the code
+# read .patch files and overwrite with zrepl appropriate changes
+# apply .patch to zrepls in working directory, flag any conflicts
+# make this code modular, with a gui
 #-------MAIN CALL-----
 main()
