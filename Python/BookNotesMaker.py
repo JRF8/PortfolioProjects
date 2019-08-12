603b7bda2a3025c8a26ce598510740fe1551dea2
Jesse Frazier <jesse.frazier.8@gmail.com>
2019-08-11 23:35:49 -0400

    made good progress on BookNotesMaker.py - appears to be working properly.

--- Python/BookNotesMaker.py
+++ Python/BookNotesMaker.py
@@ -1,37 +1,52 @@
 import string
 import os
+import datetime
 
 def main():
     f1, pages, freeTxt = promptInput()
     if f1 == False:
         print("BookNotes cancelled")
+    elif f1 == "None":
+        print("BookNotes cancelled")
     else:
-        getDateTime()
+        tStamp = getDateTime()
+        f1.write(tStamp + "\n")
+        f1.write("Pages: " + pages + "\n")
+        f1.write("Notes: " + freeTxt + "\n")
 
 def promptInput():
     try:
         f1 = promptFileLoop()
-        pages = prompt("Enter the pages read: ")
-        freeTxt = prompt("Enter your reading notes: ")
-        return f1, pages, freeTxt
+        if f1 == "Cancel":
+            print("Cancelling......")
+            return False, False, False
+        else:
+            pages = input("Enter the pages read: ")
+            freeTxt = input("Enter your reading notes: ")
+            return f1, pages, freeTxt
     except NameError:
         print("Cancelling......")
         return False, False, False
 
 def promptFileLoop():
+    print("in prompt file loop")
     fileFlg = False
     cancelFlg = False
     # will use flags to indicate still accepting input
-    while (fileFlg == False) and (cancelFlag == False)
-        title = prompt("Enter a book title: ")
-        if title = "":
-            cancelFlag == True
-        else
+    while (fileFlg == False) and (cancelFlg == False):
+        print("while loop....")
+        title = input("Enter a book title: ")
+        if title == "":
+            print("")
+            cancelFlag = True
+        else:
+            print("hereee in the else")
             fname = convertTitleToFile(title)
             f1 = openFile(fname)
-            if f1 == "None"
+            if f1 == "None":
                 cancelFlg = True
-            else
+                return "Cancel"
+            else:
                 fileFlg = True
                 return f1
 
@@ -39,15 +54,21 @@ def convertTitleToFile(title):
     return title.lower().replace(' ','_') + ".txt"
 
 def openFile(fname):
-    fullPath = '~/Documents/BookNotes/' + fname
+    fullPath = '/Users/jessefrazier/Documents/BookNotes/' + fname
     if os.path.exists(fullPath):
-        f1 = open(fullPath,'a')
+        f1 = open(fullPath,'a+')
+        return f1
     else:
-        decideNew = prompt("File doesn't exist, open new? ('yes' if ok)")
+        decideNew = input("File doesn't exist, open new? ('yes' if ok)")
         decideNew = str(decideNew).lower()
         if decideNew == "yes":
-            f1 = open(fullPath,'w')
-        else
+            f1 = open(fullPath,'w+')
+            return f1
+        else:
             return "None"
 
 def getDateTime():
+    currentDT = datetime.datetime.now()
+    return currentDT.strftime("%H:%M %d-%b-%y")
+
+main()
